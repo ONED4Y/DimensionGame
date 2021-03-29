48701e69270829ee4424a79f33994aa2d70164b6
[ONED4Y] <[eintagzhang@icloud.com]>
2021-03-29 16:19:30 +0200

    test

--- Scripts/main.cs
+++ Scripts/main.cs
@@ -6,7 +6,7 @@ public class main: MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
 
-        Debug.Log("test!!!!!!!!!Du bist ein kek!!!!!!!!");
+        Debug.Log("test!!!!!!!!!Du bist ein kekek!!!!!!!!");
         Debug.Log("JasonsNam");
     }
 
