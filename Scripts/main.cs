26ca2da9a730b7d7fe5517dca53ce7f2bd5b507d
JasonsName <81225093+JasonsName@users.noreply.github.com>
2021-03-29 10:35:34 +0200

    Irgendwas

--- Scripts/main.cs
+++ Scripts/main.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class main: MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
-        Debug.Log("test!!!!!!!!!!!!!!!!!");
+        Debug.Log("test!!!!!!!!!Du bist ein kek!!!!!!!!");
     }
 
     // Update is called once per frame
