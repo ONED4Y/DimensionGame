638f9cd63f77af1e139fa6d6e110cdcb2a40a8f9
JasonsName <81225093+JasonsName@users.noreply.github.com>
2021-03-29 11:58:32 +0200

    Wrote movement code

--- Scripts/EntityScripts/MovementScript.cs
+++ Scripts/EntityScripts/MovementScript.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class MovementScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    public int speed;
     void Start()
     {
         
@@ -13,6 +15,11 @@ public class MovementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 movement = Vector3.zero;
+
+        movement.x = Convert.ToInt32(Input.GetKey(KeyCode.D)) - Convert.ToInt32(Input.GetKey(KeyCode.A));
+        movement.y = Convert.ToInt32(Input.GetKey(KeyCode.W)) - Convert.ToInt32(Input.GetKey(KeyCode.S));
+
+        transform.Translate(movement * Time.deltaTime * speed);
     }
 }
