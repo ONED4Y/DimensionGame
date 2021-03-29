f31fc143896194112adb052a65424786263c93df
JasonsName <81225093+JasonsName@users.noreply.github.com>
2021-03-29 12:16:54 +0200

    schönheits Op

--- Scripts/EntityScripts/MovementScript.cs
+++ Scripts/EntityScripts/MovementScript.cs
@@ -7,14 +7,12 @@ public class MovementScript : MonoBehaviour
 {
     // Start is called before the first frame update
     public int speed;
-    void Start()
-    {
+    void Start(){
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
         Vector3 movement = Vector3.zero;
 
         movement.x = Convert.ToInt32(Input.GetKey(KeyCode.D)) - Convert.ToInt32(Input.GetKey(KeyCode.A));
