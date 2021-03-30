586d7d80ba729b83f0a7be975661a098bc3eba32
JasonsName <81225093+JasonsName@users.noreply.github.com>
2021-03-30 11:20:10 +0200

    nix

--- Scripts/EntityScripts/GridBasedMovement.cs
+++ Scripts/EntityScripts/GridBasedMovement.cs
@@ -20,6 +20,9 @@ public class GridBasedMovement: MonoBehaviour {
             positionPoint.position += new Vector3((Convert.ToInt32(Input.GetKey(KeyCode.D)) - Convert.ToInt32(Input.GetKey(KeyCode.A))) * GridDistance, (Convert.ToInt32(Input.GetKey(KeyCode.W)) - Convert.ToInt32(Input.GetKey(KeyCode.S))) * GridDistance, 0);
 
         }
+
+        
+
         transform.position = Vector3.MoveTowards(transform.position, positionPoint.position, Speed * Time.deltaTime);
         //transform.Translate(Vector3.MoveTowards(transform.position, positionPoint.position, Speed * Time.deltaTime));
     }
