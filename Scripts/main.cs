ec0ab586e0ac00c2ddc6ebdf4202d51e3e650f45
JasonsName <81225093+JasonsName@users.noreply.github.com>
2021-03-30 08:22:18 +0200

    nixdukek

--- Scripts/main.cs
+++ Scripts/main.cs
@@ -6,8 +6,8 @@ public class main: MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
 
-        Debug.Log("JASONISTDUMM");
-        Debug.Log("JasonsNam");
+        Debug.Log("YITIAN IST EIN KLEINER DUMMER KEK!!!!");
+        Debug.Log("ONED4Y");
     }
 
     // Update is called once per frame
