ee3e87ea48947d6e9ac9cd22294ec192c28e8837
JasonsName <81225093+JasonsName@users.noreply.github.com>
2021-03-31 12:34:01 +0200

    Camera Script

--- Scripts/EntityScripts/CameraController.cs
+++ Scripts/EntityScripts/CameraController.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Transform cam;
+    public Transform Up;
+    public Transform Down;
+    public Transform Left;
+    public Transform Right;
+    public int Speed = 5;
     void Start()
     {
         
@@ -13,6 +19,19 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 movement = Vector3.zero;
+        if(Input.GetKey(KeyCode.W)){
+            cam.position = Vector3.MoveTowards(cam.position, Up.position, Speed * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.A)){
+            cam.position = Vector3.MoveTowards(cam.position, Left.position, Speed * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.S)){
+            cam.position = Vector3.MoveTowards(cam.position, Down.position, Speed * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.D)){
+            cam.position = Vector3.MoveTowards(cam.position, Right.position, Speed * Time.deltaTime);
+        }
+
     }
 }
