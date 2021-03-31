6d24a4ea9fbeb3156bab1a9968b925284ab043ef
[ONED4Y] <[eintagzhang@icloud.com]>
2021-03-31 20:07:58 +0200

    untermensch jason

--- Scripts/EntityScripts/CameraController.cs
+++ Scripts/EntityScripts/CameraController.cs
@@ -2,36 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
-{
-    // Start is called before the first frame update
+public class CameraController: MonoBehaviour {
+
     public Transform cam;
     public Transform Up;
     public Transform Down;
     public Transform Left;
     public Transform Right;
     public int Speed = 5;
-    void Start()
-    {
-        
-    }
+
+    // Start is called before the first frame update
+    void Start() {}
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         Vector3 movement = Vector3.zero;
-        if(Input.GetKey(KeyCode.W)){
+        if(Input.GetKey(KeyCode.W)) {
             cam.position = Vector3.MoveTowards(cam.position, Up.position, Speed * Time.deltaTime);
         }
-        if(Input.GetKey(KeyCode.A)){
+        if(Input.GetKey(KeyCode.A)) {
             cam.position = Vector3.MoveTowards(cam.position, Left.position, Speed * Time.deltaTime);
         }
-        if(Input.GetKey(KeyCode.S)){
+        if(Input.GetKey(KeyCode.S)) {
             cam.position = Vector3.MoveTowards(cam.position, Down.position, Speed * Time.deltaTime);
         }
-        if(Input.GetKey(KeyCode.D)){
+        if(Input.GetKey(KeyCode.D)) {
             cam.position = Vector3.MoveTowards(cam.position, Right.position, Speed * Time.deltaTime);
         }
-
     }
 }
