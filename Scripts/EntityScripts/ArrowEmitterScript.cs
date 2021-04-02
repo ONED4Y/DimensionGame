078ae8f7daa18334119c09cfc1171e3a0da9e140
JasonsName <81225093+JasonsName@users.noreply.github.com>
2021-04-02 16:06:25 +0200

    dumme sachen

--- Scripts/EntityScripts/ArrowEmitterScript.cs
+++ Scripts/EntityScripts/ArrowEmitterScript.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ArrowEmitterScript: MonoBehaviour {
@@ -23,13 +21,27 @@ public class ArrowEmitterScript: MonoBehaviour {
             Rigidbody2D rb = Arrow.GetComponent<Rigidbody2D>();
             //Arrow.GetComponent<ArrowScript>.Move();
             //Arrow.GetComponent<ArrowScript>
-            speed = LoadedTime * speed;
+            /*
+            speed = LoadedTime * speedMultiplier;
+            
+            if(LoadedTime > 1.5F) {
+                speed = 1.5F;
+            }
+            Debug.Log(speed);
+            */
+            if(LoadedTime <= 2) {
+                speed = 0.1F;
+            }
+            if(LoadedTime >2 && LoadedTime <= 3) {
+                speed = 0.5F;
+            }
             if(LoadedTime > 3) {
-                speed = 3;
-            }    
-            rb.AddForce(transform.right * speed, ForceMode2D.Impulse);
+                speed = 1F;
+            }
+            Debug.Log(speed);
+            Debug.Log(LoadedTime);
+            rb.AddForce(transform.right * speed * Time.deltaTime, ForceMode2D.Impulse);
             //Arrow.transform.Translate(new Vector3(speed, 0, 0) * Time.deltaTime);
-
         }
     }
 }
