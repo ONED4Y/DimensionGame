14b07ef4c8b398802229818422fdf0fb9ed40cb7
JasonsName <81225093+JasonsName@users.noreply.github.com>
2021-04-03 15:02:58 +0200

    Revert "slow down while aiming"
    This reverts commit b612521fd8d4ad2b0cd2b9969b424fce867eed6a.

--- Scripts/EntityScripts/GridBasedMovement.cs
+++ Scripts/EntityScripts/GridBasedMovement.cs
@@ -5,7 +5,7 @@ using System;
 
 public class GridBasedMovement: MonoBehaviour {
 
-    public float Speed = 3;
+    public int Speed = 3;
     public float GridDistance = 1;
     public Transform positionPoint;
     // Start is called before the first frame update
@@ -25,13 +25,5 @@ public class GridBasedMovement: MonoBehaviour {
 
         transform.position = Vector3.MoveTowards(transform.position, positionPoint.position, Speed * Time.deltaTime);
         //transform.Translate(Vector3.MoveTowards(transform.position, positionPoint.position, Speed * Time.deltaTime));
-
-        if(Input.GetKey(KeyCode.I)){
-            Speed = 0.5F;
-        }else {
-            Speed = 3;
-        }
-
-
     }
 }
