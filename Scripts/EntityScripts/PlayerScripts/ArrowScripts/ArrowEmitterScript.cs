42295a62d31a38d57644426360c31f42c6cc03b9
JasonsName <81225093+JasonsName@users.noreply.github.com>
2021-04-04 11:04:14 +0200

    Arrow spam disabled

--- Scripts/EntityScripts/PlayerScripts/ArrowScripts/ArrowEmitterScript.cs
+++ Scripts/EntityScripts/PlayerScripts/ArrowScripts/ArrowEmitterScript.cs
@@ -17,34 +17,36 @@ public class ArrowEmitterScript: MonoBehaviour {
         }
         if(Input.GetKeyUp(KeyCode.I)) {
             LoadedTime = Time.time - KeyTimeDown;
-            GameObject Arrow = Instantiate(arrow, transform.position, transform.rotation);
-            Rigidbody2D rb = Arrow.GetComponent<Rigidbody2D>();
-            //Arrow.GetComponent<ArrowScript>.Move();
-            //Arrow.GetComponent<ArrowScript>
-            /*
-            speed = LoadedTime * speedMultiplier;
-            
-            if(LoadedTime > 1.5F) {
-                speed = 1.5F;
+            if(LoadedTime > 0.2F) {
+                GameObject Arrow = Instantiate(arrow, transform.position, transform.rotation);
+                Rigidbody2D rb = Arrow.GetComponent<Rigidbody2D>();
+                //Arrow.GetComponent<ArrowScript>.Move();
+                //Arrow.GetComponent<ArrowScript>
+                /*
+                speed = LoadedTime * speedMultiplier;
+                
+                if(LoadedTime > 1.5F) {
+                    speed = 1.5F;
+                }
+                Debug.Log(speed);
+                */
+                if(LoadedTime <= 1) {
+                    speed = 50F;
+                }
+                if(LoadedTime > 1 && LoadedTime <= 2) {
+                    speed = 100F;
+                }
+                if(LoadedTime > 2) {
+                    speed = 500F;
+                }
+                Debug.Log(speed);
+                Debug.Log(LoadedTime);
+                Debug.Log(Arrow);
+                Debug.Log(rb);
+                //rb.velocity = transform.right * speed * Time.deltaTime;
+                rb.AddForce(transform.right * speed * Time.deltaTime * speedMultiplier, ForceMode2D.Impulse);
+                //Arrow.transform.Translate(new Vector3(speed, 0, 0) * Time.deltaTime);
             }
-            Debug.Log(speed);
-            */
-            if(LoadedTime <= 1) {
-                speed = 50F;
-            }
-            if(LoadedTime > 1 && LoadedTime <= 2) {
-                speed = 100F;
-            }
-            if(LoadedTime > 2) {
-                speed = 500F;
-            }
-            Debug.Log(speed);
-            Debug.Log(LoadedTime);
-            Debug.Log(Arrow);
-            Debug.Log(rb);
-            //rb.velocity = transform.right * speed * Time.deltaTime;
-            rb.AddForce(transform.right * speed * Time.deltaTime * speedMultiplier, ForceMode2D.Impulse);
-            //Arrow.transform.Translate(new Vector3(speed, 0, 0) * Time.deltaTime);
         }
     }
 }
