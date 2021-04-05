dc17838fed82b41980456e78ff13b8d0d222ce8b
[ONED4Y] <[eintagzhang@icloud.com]>
2021-04-05 14:43:44 +0200

    ICH BIN FERTIG KEIKEKEIKE

--- Scripts/EntityScripts/HostileMobs/HostileEntityClasses.cs
+++ Scripts/EntityScripts/HostileMobs/HostileEntityClasses.cs
@@ -10,6 +10,20 @@ public class MobFeatures {
         else if(MobCoord > PlayerCoord) { return MobCoord - 1F; }
         else { return PlayerCoord; }
     }*/
+    public static Vector3 WalkDir(Vector3 CurrentPos, int R) {
+        switch(R) {
+            case 1:
+                return new Vector3(CurrentPos.x + 1, CurrentPos.y, 0);
+            case 2:
+                return new Vector3(CurrentPos.x - 1, CurrentPos.y, 0);
+            case 3:
+                return new Vector3(CurrentPos.x, CurrentPos.y + 1, 0);
+            case 4:
+                return new Vector3(CurrentPos.x, CurrentPos.y - 1, 0);
+            default:
+                return CurrentPos;
+        }
+    }
 }
 
 public class Mob1: MobFeatures {
@@ -18,19 +32,26 @@ public class Mob1: MobFeatures {
     public float Size = 1F;
     public float Speed = 20F;
     public bool IsMelee = true;
-    public float playerIsInRange = 1.5F;
+    public float PlayerIsInRange = 1.5F;
     public IEnumerator GoToPlayer(Transform TF) {
+        int RandomWalkDir = Mathf.RoundToInt(Random.Range(1, 5));
+        float LifeTime = Time.time;
+
         while(true) {
             GameObject Player = GameObject.Find("Player");
             Vector3 PlayerPos = Player.transform.position;
-            if(Vector3.Distance(TF.position, PlayerPos) > playerIsInRange/*Diff != new Vector3(0, 0, 0)*/) {
-                //Vector3 MoveTo = new Vector3(Mathf.Round(PlayerPos.x), Mathf.Round(PlayerPos.y), 0);
-                //MoveTo.x = MoveDir(TF.position.x, PlayerPos.x);
-                //MoveTo.y = MoveDir(TF.position.y, PlayerPos.y);
+            float Dist = Vector3.Distance(TF.position, PlayerPos);
+
+            if(Dist > PlayerIsInRange && Dist < 15 && LifeTime > 1.5/*Diff != new Vector3(0, 0, 0)*/) {
                 TF.position = Vector3.MoveTowards(TF.position, PlayerPos, Speed * Time.deltaTime);
-                Debug.Log(Speed * Time.deltaTime);
+            } else if(Dist >= 15 && LifeTime > 1.5) {
+                TF.position = Vector3.MoveTowards(TF.position, WalkDir(TF.position, RandomWalkDir), Speed * Time.deltaTime);
             }
-            Debug.Log("B");
+
+            if(Mathf.Round(Random.Range(1, 6)) == 1) {
+                RandomWalkDir = Mathf.RoundToInt(Random.Range(1, 5));
+            }
+            LifeTime = Time.time;
             yield return new WaitForSecondsRealtime(.1F);
         }
     }
--- Scripts/EntityScripts/MobSpawningScripts/SpawnMob1.cs
+++ Scripts/EntityScripts/MobSpawningScripts/SpawnMob1.cs
@@ -9,13 +9,7 @@ public class SpawnMob1: MonoBehaviour {
     }
     IEnumerator RandomMobSpawn() {
         while(true) {
-            float Spawn = 1;//Random.Range(1, 10);
-            if(Spawn == 1) {
-                //ExampleMob Mob = new ExampleMob();
-                /*GameObject MobGO = */
-                Instantiate(ExampleMobPrefab, Vector3.zero, transform.rotation);
-                //StartCoroutine(Mob.GoToPlayer(MobGO, MobGO.transform));
-            }
+            Instantiate(ExampleMobPrefab, Vector3.zero, transform.rotation);
             yield return new WaitForSeconds(10F);
         }
     } // create an own script for each mob so that the coroutine will work
