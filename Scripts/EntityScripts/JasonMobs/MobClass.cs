5156d92613f77bba70436778d05144eee1a61ccf
JasonsName <81225093+JasonsName@users.noreply.github.com>
2021-04-05 20:54:50 +0200

    random movement

--- Scripts/EntityScripts/JasonMobs/MobClass.cs
+++ Scripts/EntityScripts/JasonMobs/MobClass.cs
@@ -7,7 +7,22 @@ public class Mob: MonoBehaviour {
     public int JHp;
     public int JSpeed;
 
-    public void JDoRandomMovement() {
-        
+    public Vector3 JDoRandomMovement(int _Speed, int _Length) {
+        int RandDir = Mathf.RoundToInt(Random.Range(0, 3));
+        switch(RandDir) {
+            case 0: 
+                //Debug.Log("keek1");
+                return Vector3.MoveTowards(transform.position, new Vector3(transform.position.x + _Length, 0, 0), _Speed * Time.deltaTime);
+            case 1: 
+                //Debug.Log("keek2");
+                return Vector3.MoveTowards(transform.position, new Vector3(transform.position.x - _Length, 0, 0), _Speed * Time.deltaTime);
+            case 2: 
+                //Debug.Log("keek3");
+                return Vector3.MoveTowards(transform.position, new Vector3(0, transform.position.y + _Length, 0), _Speed * Time.deltaTime);
+            case 3: 
+                //Debug.Log("keek4");
+                return Vector3.MoveTowards(transform.position, new Vector3(0, transform.position.y - _Length, 0), _Speed * Time.deltaTime);
+            default: return new Vector3(0, 0, 0);
+        }
     }
 }
--- Scripts/EntityScripts/JasonMobs/MobInstanceScripts/SnoggerScript.cs
+++ Scripts/EntityScripts/JasonMobs/MobInstanceScripts/SnoggerScript.cs
@@ -9,21 +9,27 @@ public class SnoggerScript: Mob {
     // Start is called before the first frame update
     void Start() {
         Debug.Log("Wuuuaaaah!! Ich LEBE!!");
+        while(true) {
+            JDoStuff();
+        }
     }
-
-    // Update is called once per frame
-    void Update() {
+    IEnumerator RandomMovement() {
+        JDoRandomMovement(2, 1);
+        yield return new WaitForSeconds(3);
+    }
+    void JDoStuff() {
         Transform JplayerTransf = GameObject.Find("Player").transform;
         //Debug.Log(Vector3.Distance(transform.position, JplayerTransf.position));
         if(Vector3.Distance(transform.position, JplayerTransf.position) < JDetectPlayerRange && Vector3.Distance(transform.position, JplayerTransf.position) > JAtPlayerRange) {
             Vector3 RoadToPlayer = JplayerTransf.position - transform.position;
             Debug.Log("Ich bin du und du bist ein kek!");
-            StartCoroutine("JGoToPlayer", JplayerTransf.position);
             transform.position = Vector3.MoveTowards(transform.position, JplayerTransf.position, JSpeed * Time.deltaTime);
             //transform.Translate((JplayerTransf.position - transform.position) * Time.deltaTime * JSpeed);
         }
         else {
-            
+            Mathf.RoundToInt(transform.position.x);
+            Mathf.RoundToInt(transform.position.y);
+            StartCoroutine("RandomMovement");
         }
     }
 }
