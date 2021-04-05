ba1341290f02ccd57b5f9ba2218dab9051cba782
JasonsName <81225093+JasonsName@users.noreply.github.com>
2021-04-05 18:58:58 +0200

    Snogger script

--- Scripts/EntityScripts/JasonMobs/MobInstanceScripts/SnoggerScript.cs
+++ Scripts/EntityScripts/JasonMobs/MobInstanceScripts/SnoggerScript.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class SnoggerScript : MonoBehaviour
 {
+    public float JDetectPlayerRange;
+    public float JAtPlayerRange;
+    public LayerMask enemyLayers;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Wuuuaaaah!! Ich LEBE!!");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Transform JplayerTransf = GameObject.Find("Player").transform;
+        if(Vector3.Distance(transform.position, JplayerTransf.position) < JDetectPlayerRange && Vector3.Distance(transform.position, JplayerTransf.position) > JAtPlayerRange) {
+            Vector3 RoadToPlayer = JplayerTransf.position - transform.position;
+            Debug.Log("Ich bin du und du bist ein kek!");
+        }
     }
 }
