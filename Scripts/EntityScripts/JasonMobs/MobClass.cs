11cac300997fecefabee7fb8622ce366d3d081ef
JasonsName <81225093+JasonsName@users.noreply.github.com>
2021-04-05 19:13:27 +0200

    nix eigentlich

--- Scripts/EntityScripts/JasonMobs/MobClass.cs
+++ Scripts/EntityScripts/JasonMobs/MobClass.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Mob {
+public class Mob: MonoBehaviour {
     public int JDamage;
     public int JHp;
     public int JSpeed;
