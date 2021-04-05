358551f161118f9fbe8398017d124e66ffe5c3a7
[ONED4Y] <[eintagzhang@icloud.com]>
2021-04-05 11:42:07 +0200

    unkooperativ unkooperativ unkooperativ unkooperati

--- Scripts/EntityScripts/HostileMobs/HostileEntityClasses.cs
+++ Scripts/EntityScripts/HostileMobs/HostileEntityClasses.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MobFeatures {
-    public static float MoveDir(float MobCoord, float PlayerCoord) {
+    /*public static float MoveDir(float MobCoord, float PlayerCoord) {
         if(Mathf.Abs(PlayerCoord - MobCoord) < 1) { return PlayerCoord; }
         MobCoord = Mathf.Round(MobCoord); PlayerCoord = Mathf.Round(PlayerCoord);
         if(MobCoord < PlayerCoord) { return MobCoord + 1F; }
         else if(MobCoord > PlayerCoord) { return MobCoord - 1F; }
         else { return PlayerCoord; }
-    }
+    }*/
 }
 
 public class Mob1: MobFeatures {
@@ -24,10 +24,10 @@ public class Mob1: MobFeatures {
             GameObject Player = GameObject.Find("Player");
             Vector3 PlayerPos = Player.transform.position;
             while(Vector3.Distance(TF.position, PlayerPos) > playerIsInRange/*Diff != new Vector3(0, 0, 0)*/) {
-                Vector3 MoveTo = new Vector3(Mathf.Round(PlayerPos.x), Mathf.Round(PlayerPos.y), 0);
-                MoveTo.x = MoveDir(TF.position.x, PlayerPos.x);
-                MoveTo.y = MoveDir(TF.position.y, PlayerPos.y);
-                TF.position = Vector3.MoveTowards(TF.position, MoveTo, Speed * Time.deltaTime);
+                //Vector3 MoveTo = new Vector3(Mathf.Round(PlayerPos.x), Mathf.Round(PlayerPos.y), 0);
+                //MoveTo.x = MoveDir(TF.position.x, PlayerPos.x);
+                //MoveTo.y = MoveDir(TF.position.y, PlayerPos.y);
+                TF.position = Vector3.MoveTowards(TF.position, PlayerPos, Speed * Time.deltaTime);
                 break;
             }
             yield return new WaitForSeconds(.5F);
