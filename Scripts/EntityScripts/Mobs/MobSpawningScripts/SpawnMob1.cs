9ad00503eaa32fa4bb8617394f4deed30cc84e46
JasonsName <81225093+JasonsName@users.noreply.github.com>
2021-04-06 10:29:01 +0200

    snooger spawner

--- Scripts/EntityScripts/Mobs/MobSpawningScripts/SpawnMob1.cs
+++ Scripts/EntityScripts/Mobs/MobSpawningScripts/SpawnMob1.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class SpawnMob1: MonoBehaviour {
     public GameObject ExampleMobPrefab;
+    public GameObject Snogger;
     void Start() {
         StartCoroutine("RandomMobSpawn");
     }
@@ -13,4 +14,10 @@ public class SpawnMob1: MonoBehaviour {
             yield return new WaitForSeconds(10F);
         }
     } // create an own script for each mob so that the coroutine will work
+    IEnumerator RandomSnoggerSpawn() {
+        while(true) {
+            Instantiate(Snogger, Vector3.zero, transform.rotation);
+            yield return new WaitForSeconds(10F);
+        }
+    }
 }
