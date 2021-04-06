5031fd9db8050f861734aa073cf8464260d3891f
JasonsName <81225093+JasonsName@users.noreply.github.com>
2021-04-06 10:20:22 +0200

    kp

--- Scripts/EntityScripts/JasonMobs/MobInstanceScripts/SnoggerScript.cs
+++ Scripts/EntityScripts/JasonMobs/MobInstanceScripts/SnoggerScript.cs
@@ -10,7 +10,6 @@ public class SnoggerScript: Mob {
     public float JGridDistance;
     private bool JIsMoving;
     public Transform JpositionPoint;
-    // Start is called before the first frame update
     void Start() {
         Debug.Log("Wuuuaaaah!! Ich LEBE!!");
         JpositionPoint.parent = null;
@@ -19,28 +18,19 @@ public class SnoggerScript: Mob {
         yield return new WaitForSeconds(3);
         JIsMoving = false;
     }
-    void JDoStuff() {
+    void Update() {
         Transform JplayerTransf = GameObject.Find("Player").transform;
-        //Debug.Log(Vector3.Distance(transform.position, JplayerTransf.position));
         if(Vector3.Distance(transform.position, JplayerTransf.position) < JDetectPlayerRange && Vector3.Distance(transform.position, JplayerTransf.position) > JAtPlayerRange) {
             JpositionPoint.parent = gameObject.transform;
-            Vector3 RoadToPlayer = JplayerTransf.position - transform.position;
-            Debug.Log("Ich bin du und du bist ein kek!");
             transform.position = Vector3.MoveTowards(transform.position, JplayerTransf.position, JSpeed * Time.deltaTime);
-            //transform.Translate((JplayerTransf.position - transform.position) * Time.deltaTime * JSpeed);
-        }
-        else {
+        }else {
             JpositionPoint.parent = null;
             if(Vector3.Distance(transform.position, JpositionPoint.position) <= JGridDistance * 0.05F && !JIsMoving) {
                 JIsMoving = true;
                 StartCoroutine("Wait", 3);
                 JpositionPoint.position = JDoRandomMovement(1);
-                //StartCoroutine("RandomMovement");
             }
             transform.position = Vector3.MoveTowards(transform.position, JpositionPoint.position, JSpeed * Time.deltaTime);
         }
-    }
-    void Update() {
-        JDoStuff();
     }
 }
