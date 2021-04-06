027d4127d6d258cd408c55d818d5e8362e83f846
JasonsName <81225093+JasonsName@users.noreply.github.com>
2021-04-06 13:02:59 +0200

    Revert "Made animated sprites"
    This reverts commit de550b14a34a3b3c62a456006962a4a8eef1eb5b.

--- Scripts/EntityScripts/PlayerScripts/MovementScripts/GridBasedMovement.cs
+++ Scripts/EntityScripts/PlayerScripts/MovementScripts/GridBasedMovement.cs
@@ -8,7 +8,6 @@ public class GridBasedMovement: MonoBehaviour {
     public float Speed = 3;
     public float GridDistance = 1;
     public Transform positionPoint;
-    public LayerMask CollisionLayers;
     // Start is called before the first frame update
     void Start() {
         positionPoint.parent = null;
@@ -18,10 +17,7 @@ public class GridBasedMovement: MonoBehaviour {
     void Update() {
         if(Vector3.Distance(transform.position, positionPoint.position) <= GridDistance * 0.05F) {
             
-            if(!Physics2D.OverlapCircle(positionPoint.position + new Vector3((Convert.ToInt32(Input.GetKey(KeyCode.D)) - Convert.ToInt32(Input.GetKey(KeyCode.A))) * GridDistance, (Convert.ToInt32(Input.GetKey(KeyCode.W)) - Convert.ToInt32(Input.GetKey(KeyCode.S))) * GridDistance, 0), 0.2F, CollisionLayers)) {
-                positionPoint.position += new Vector3((Convert.ToInt32(Input.GetKey(KeyCode.D)) - Convert.ToInt32(Input.GetKey(KeyCode.A))) * GridDistance, (Convert.ToInt32(Input.GetKey(KeyCode.W)) - Convert.ToInt32(Input.GetKey(KeyCode.S))) * GridDistance, 0);
-            }
-            
+            positionPoint.position += new Vector3((Convert.ToInt32(Input.GetKey(KeyCode.D)) - Convert.ToInt32(Input.GetKey(KeyCode.A))) * GridDistance, (Convert.ToInt32(Input.GetKey(KeyCode.W)) - Convert.ToInt32(Input.GetKey(KeyCode.S))) * GridDistance, 0);
 
         }
         transform.position = Vector3.MoveTowards(transform.position, positionPoint.position, Speed * Time.deltaTime);
